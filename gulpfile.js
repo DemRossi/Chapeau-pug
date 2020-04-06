8779627929f4da3887f0af7794e9d0b5550320de
Wesley Wijsen <wesleywijsen@hotmail.com>
2020-04-06 22:30:46 +0200

    Start of project

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const { src, dest, watch, parallel, series } = require('gulp'); 
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const minify = require('gulp-minify');
+const nodemon = require('gulp-nodemon');
+
+//compile all SASS to CSS
+function sassToCss(done){
+    src("./public/source/sass/app.scss")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(dest("./public/dist/css/"))
+        .pipe(browserSync.stream());
+
+    done();
+}
+//minify all JS files
+function minifyJs(done){
+    src("./public/javascripts/*.js")
+        .pipe(minify())
+        .pipe(dest("./public/javascripts/minified/"));
+
+    done();
+}
+
+//start local server via nodemon
+function startNodemon(done){
+    nodemon({
+        script: './bin/www',
+        ext: "pug js",
+        env: { 'NODE_ENV': 'development' },
+        done: done
+    });
+}
+
+  
+module.exports.default = series(sassToCss, minifyJs, startNodemon);
