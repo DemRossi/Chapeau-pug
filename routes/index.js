9d4fa4bacedc7efcfb0edb2153924e079564cc9b
Wesley Wijsen <wesleywijsen@hotmail.com>
2020-04-07 13:35:36 +0200

    Made a header component for navigation

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chapeau' });
+  res.render('index', { title: 'Chapeau - Home' });
 });
 router.get('/login', (req, res,next)=>{
   res.render('login', {title: 'Chapeau - Log in'} )
