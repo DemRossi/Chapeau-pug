d4b2c5474d7e5fc06c84de5b7c3934ba8393f775
Wesley Wijsen <wesleywijsen@hotmail.com>
2020-04-12 13:41:17 +0200

    Changed app.js to home.js in javascript

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,21 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
+const passport = require('../passport/passport')
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Chapeau - Home" })
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Chapeau - Home' })
 })
-router.get("/login", (req, res, next) => {
-  res.render("login", { title: "Chapeau - Log in" })
+router.get('/login', (req, res, next) => {
+  res.render('login', { title: 'Chapeau - Log in' })
 })
-router.get("/signup", (req, res, next) => {
-  res.render("signup", { title: "Chapeau - signup" })
+router.get('/signup', (req, res, next) => {
+  res.render('signup', { title: 'Chapeau - signup' })
 })
-router.get("/create", (req, res, next) => {
-  res.render("create", { title: "Chapeau - Create Lobby" })
+router.get('/create', (req, res, next) => {
+  res.render('create', { title: 'Chapeau - Create Lobby' })
 })
-router.get("/lobby", (req, res, next) => {
-  res.render("lobby", { title: "Chapeau - Inside lobby" })
+router.get('/lobby', (req, res, next) => {
+  res.render('lobby', { title: 'Chapeau - Inside lobby' })
 })
 module.exports = router
