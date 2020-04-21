759b966a930dcc9cf07c2e5cd6ee73718da1fedb
Wesley Wijsen <wesleywijsen@hotmail.com>
2020-04-21 17:40:16 +0200

    Made check for full lobby

--- public/javascripts/ajax_J.js
+++ public/javascripts/ajax_J.js
@@ -3,49 +3,69 @@ window.onload = (event) => {
   let lobbiesContainer = document
     .querySelector('.content__wrapper--bottom-middle')
     .addEventListener('click', (e) => {
-      if (e.target.matches('.btn--join')) {
-        // Get lobby id
-        let lobby_id = e.target.parentElement.parentElement.dataset.id
+      let playersinside = parseInt(
+        e.target.parentElement.previousElementSibling.previousElementSibling
+          .firstElementChild.firstElementChild.innerHTML
+      )
 
-        // Get user data
-        let user_id = localStorage.getItem('user_id')
-        let username = localStorage.getItem('username')
-        let gamesplayed = localStorage.getItem('gamesplayed')
-        let gameswon = localStorage.getItem('gameswon')
+      let playersamount = parseInt(
+        e.target.parentElement.previousElementSibling.previousElementSibling
+          .firstElementChild.lastElementChild.innerHTML
+      )
 
-        console.log(lobby_id)
-        console.log(user_id)
-        console.log(username)
-        console.log(gamesplayed)
-        console.log(gameswon)
+      console.log(playersinside + playersamount)
+      console.log(playersamount)
 
-        // Start join fetch /lobby/${json.data[i]._id}
+      if (playersinside >= playersamount) {
+        // console.log('cant join!!!!')
+        // Full Lobby error
+      } else {
+        console.log('can join!!!!')
+        // proceed to join
+        // if (e.target.matches('.btn--join')) {
+        //   // Get lobby id
+        //   let lobby_id = e.target.parentElement.parentElement.dataset.id
 
-        fetch(`/api/v1/lobby/${lobby_id}`, {
-          method: 'put',
-          headers: {
-            Accept: 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + localStorage.getItem('token'),
-          },
-          body: JSON.stringify({
-            uid: user_id,
-            username: username,
-            gamesplayed: gamesplayed,
-            gameswon: gameswon,
-          }),
-        })
-          .then((response) => {
-            return response.json()
-          })
-          .then((json) => {
-            if (json.status == 'success') {
-              //redirect to /lobby/:id
-              window.location.href = `/lobby/${lobby_id}`
-            }
-          })
+        //   // Get user data
+        //   let user_id = localStorage.getItem('user_id')
+        //   let username = localStorage.getItem('username')
+        //   let gamesplayed = localStorage.getItem('gamesplayed')
+        //   let gameswon = localStorage.getItem('gameswon')
+
+        //   // console.log(lobby_id)
+        //   // console.log(user_id)
+        //   // console.log(username)
+        //   // console.log(gamesplayed)
+        //   // console.log(gameswon)
+
+        //   // Start join fetch
+
+        //   fetch(`/api/v1/lobby/${lobby_id}`, {
+        //     method: 'put',
+        //     headers: {
+        //       Accept: 'application/json, text/plain, */*',
+        //       'Content-Type': 'application/json',
+        //       Authorization: 'Bearer ' + localStorage.getItem('token'),
+        //     },
+        //     body: JSON.stringify({
+        //       // Put user data in json
+        //       uid: user_id,
+        //       username: username,
+        //       gamesplayed: gamesplayed,
+        //       gameswon: gameswon,
+        //     }),
+        //   })
+        //     .then((response) => {
+        //       return response.json()
+        //     })
+        //     .then((json) => {
+        //       if (json.status == 'success') {
+        //         //redirect to /lobby/:id
+        //         window.location.href = `/lobby/${lobby_id}`
+        //       }
+        //     })
+        // }
       }
-
       e.preventDefault()
     })
   event.preventDefault()
