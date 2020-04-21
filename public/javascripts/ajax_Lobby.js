fbb171062a7a80e043f8a93f966f2ccabd88b678
Wesley Wijsen <wesleywijsen@hotmail.com>
2020-04-21 21:52:26 +0200

    Removed some comments

--- public/javascripts/ajax_Lobby.js
+++ public/javascripts/ajax_Lobby.js
@@ -13,7 +13,7 @@ fetch(`http://localhost:3000/api/v1/lobby/${lobby_id}`, {
     return result.json()
   })
   .then((json) => {
-    console.log(json)
+    // console.log(json)
     if (json['status'] == 'success') {
       let lobbyname = json.data.lobbyname
       document.querySelector('.lobbyname').innerHTML = lobbyname
@@ -22,15 +22,15 @@ fetch(`http://localhost:3000/api/v1/lobby/${lobby_id}`, {
         let playerWrapper = document.createElement('div')
         playerWrapper.classList.add('content__list', 'content__list--lobby')
         let playerTemplate = `
-        <div class="content__list_profilepic_wrapper--big content__list_profilepic--big"></div>
-        <h4 class="content__list_username">${json.data.playersinside[i].username}</h4>
-        <div class="content__list_amountPlays">
-            <h6><span class="content__list_amountPlays_amount">${json.data.playersinside[i].gamesplayed}</span> games played</h6>
-        </div>
-        <div class="content__list_amountWins">
-            <h6><span class="content__list_amountWins_amount">${json.data.playersinside[i].gameswon}</span> games won</h6>
-        </div>
-      `
+          <div class="content__list_profilepic_wrapper--big content__list_profilepic--big"></div>
+          <h4 class="content__list_username">${json.data.playersinside[i].username}</h4>
+          <div class="content__list_amountPlays">
+              <h6><span class="content__list_amountPlays_amount">${json.data.playersinside[i].gamesplayed}</span> games played</h6>
+          </div>
+          <div class="content__list_amountWins">
+              <h6><span class="content__list_amountWins_amount">${json.data.playersinside[i].gameswon}</span> games won</h6>
+          </div>
+        `
         playerWrapper.innerHTML = playerTemplate
         const playerList = document.querySelector(
           '.content__wrapper--middle-middle'
@@ -38,7 +38,7 @@ fetch(`http://localhost:3000/api/v1/lobby/${lobby_id}`, {
         playerList.appendChild(playerWrapper)
       }
     } else if (json['status'] == 'failed') {
-      console.log('here')
+      // console.log('here')
       const title = 'Oops'
       document.querySelector('.lobbyname').innerHTML = title
       document.querySelector('.btn--ready').remove()
@@ -52,10 +52,10 @@ fetch(`http://localhost:3000/api/v1/lobby/${lobby_id}`, {
         'show'
       )
       let alertTemplate = `
-          <strong>Hats not found!</strong> Something went wrong with retrieving data for this lobby. Go back and try again.
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+        <strong>Hats not found!</strong> Something went wrong with retrieving data for this lobby. Go back and try again.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       `
       alert.innerHTML = alertTemplate
 
